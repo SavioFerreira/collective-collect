5183af37a8aa19d9d0cbbadf5a5f3558b8ad1c0f
Savio Ferreira <saviofc.dev@gmail.com>
2024-04-16 14:03:19 -0300

    Alterando de validade para create. nas propriedades

---
+++ frontend/src/dtos/ColetaDTO.ts
@@ -0,0 +1,14 @@
+export type ColetaDTO = {
+  
+		id: number,
+		status: string,
+		type: string,
+		gravity: string,
+		date: string,
+		image: string,
+		title: string,
+		description:string,
+		locale: string,
+		complaintId: number,
+		collaborators: [],
+	}
