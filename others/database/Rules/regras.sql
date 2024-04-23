3f1b4faaa7900aa87448f4f69085111c547cbb7d
Savio Ferreira <saviofc.dev@gmail.com>
2024-04-23 14:51:13 -0300

    incluindo script de regras root

--- others/database/Rules/regras.sql
+++ others/database/Rules/regras.sql
@@ -1,8 +1,15 @@
 insert into
-  "public"."rules" ("name")
+  "public"."role" ("id","name")
 values
-  ('PRODUCT_SELECT'),
-  ('PRODUCT_INSERT'),
-  ('PRODUCT_UPDATE'),
-  ('PRODUCT_DELETE');
- 
+  (1,'COMPLAINT_SELECT'),
+  (2,'COMPLAINT_CREATE'),
+  (3,'COMPLAINT_UPDATE'),
+  (4,'COMPLAINT_DELETE'),
+  (5,'COLLECT_SELECT'),
+  (6,'COLLECT_CREATE'),
+  (7,'COLLECT_UPDATE'),
+  (8,'COLLECT_DELETE'),
+  (9,'USER_SELECT'),
+  (10,'USER_CREATE'),
+  (11,'USER_UPDATE'),
+  (12,'USER_DELETE');
