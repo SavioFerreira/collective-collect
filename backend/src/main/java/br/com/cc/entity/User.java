6e53d6fe90c0ff2fc42c9dfc8daff2fc5dbf75c6
Savio Ferreira <saviofc.dev@gmail.com>
2024-04-24 20:08:06 -0300

    incluindo dependencia do jwt e alterando classe user para implementar classe de details do spring security

--- backend/src/main/java/br/com/cc/entity/User.java
+++ backend/src/main/java/br/com/cc/entity/User.java
@@ -1,30 +1,70 @@
 package br.com.cc.entity;
-
-import br.com.cc.security.role.Role;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import br.com.cc.enums.AuthUserRole;
 import lombok.*;
-
+import java.util.Collection;
 import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import jakarta.persistence.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private AuthUserRole role;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String name;
-	@Column(unique = true)
-	private String email;
-	private String password;
-	private boolean isAdministrator;
+    public User(String name, String password, String email, AuthUserRole role) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
 
-	@ManyToMany
-	private List<Role> roles;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == AuthUserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
 
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
