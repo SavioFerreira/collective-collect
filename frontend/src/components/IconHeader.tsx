e256ec5015aea2056b8064ee0805f00ff3534a79
Savio Ferreira <saviofc.dev@gmail.com>
2024-05-30 14:12:12 -0300

    alterando icone de reciclagem para diferenciar user admin de usuario padrao

--- frontend/src/components/IconHeader.tsx
+++ frontend/src/components/IconHeader.tsx
@@ -1,15 +1,19 @@
 import { TouchableOpacity } from "react-native";
 import { HStack, Heading, VStack } from "native-base";
-
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useAuth } from "@hooks/useAuth";
+import { RoleType } from "@enums/RoleTypesEnum";
 
 import RecycleIconSvg from '@assets/recycleLogo.svg';
-import { SafeAreaFrameContext, SafeAreaView } from "react-native-safe-area-context";
+import RecycleIconAdminSvg from '@assets/logoAdmin.svg';
 
 type Props = {
   title: string;
 }
 
 export function IconHeader({title}:Props){
+
+  const {user} = useAuth();
 
   return (
     <SafeAreaView style={{backgroundColor: 'rgb(74, 167, 255)'}}>
@@ -22,7 +26,11 @@ export function IconHeader({title}:Props){
       </VStack>
 
       <TouchableOpacity  onPress={() => {}}>
-      <RecycleIconSvg height={50} width={50} />
+      { user.role === RoleType.ADMIN ?
+        <RecycleIconAdminSvg height={50} width={50} />
+        :
+        <RecycleIconSvg height={50} width={50} />
+      }
       </TouchableOpacity>
     </HStack>
     </SafeAreaView>
