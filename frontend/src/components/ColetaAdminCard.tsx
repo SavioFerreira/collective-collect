db72f63e2bd44e0f62f42b7a6a10049c0816aa33
Savio Ferreira <saviofc.dev@gmail.com>
2024-06-14 17:52:59 -0300

    Criando esboço para tela de gerenciamento de coletas do admin

--- frontend/src/components/ColetaAdminCard.tsx
+++ frontend/src/components/ColetaAdminCard.tsx
@@ -4,25 +4,24 @@ import { Heading, HStack, Text, VStack, Icon } from 'native-base';
 import { DenunciaDTO } from "@dtos/DenunciaDTO"
 import { getGravityIcon, getStatusIcon, getTypeIcon } from 'src/functions/Icons';
 import { FormatDate } from 'src/functions/FormatDate';
+import { ColetaDTO } from '@dtos/ColetaDTO';
 
 
 type Props = TouchableOpacityProps & {
-  data: DenunciaDTO;
+  data: ColetaDTO;
 };
 
 export function ColetaAdminCard({ data, ...rest }: Props) {
 
   const status = data.status?.toLocaleLowerCase().replace("_", " ");
-  const showDate = data.complaintDate != undefined || null ? FormatDate(data.complaintDate) : "Data indisponível";
-  const author = data.author.id != undefined || null ? data.author.id : "Autor indisponível";
   const typeIcon = getTypeIcon(data.type);
   const statusIcon = getStatusIcon(data.status ?? 'Status indisponível');
   const gravityIcon = getGravityIcon(data.gravity);
 
   return (
     <TouchableOpacity {...rest}>
-      <VStack bg="coolGray.700" alignItems="center" p={1} rounded="md" mb={3} borderWidth={1} borderColor={gravityIcon.color}>
-        <HStack alignItems="center" px={4} rounded="md" mb={2}>
+      <VStack bg="cyan.900" alignItems="center" rounded="md" mb={2}>
+        <HStack alignItems="center" px={6} rounded="md" mb={2}>
           <Icon
             as={typeIcon.Component}
             name={typeIcon.name}
@@ -31,7 +30,7 @@ export function ColetaAdminCard({ data, ...rest }: Props) {
             mt={2}
           />
           <VStack flex={1}>
-            <Heading alignSelf="center" fontSize="lg" fontFamily="heading" color={gravityIcon.color} numberOfLines={1}>
+            <Heading alignSelf="center" fontSize="lg" fontFamily="heading" color={gravityIcon.color} numberOfLines={1} mt={1}>
               {data.title}
             </Heading>
             <Text alignSelf="center" fontSize="sm" fontFamily="body" color={statusIcon.color} numberOfLines={1}>
@@ -39,7 +38,6 @@ export function ColetaAdminCard({ data, ...rest }: Props) {
             </Text>
 
           </VStack>
-
           <Icon
             as={gravityIcon.Component}
             name={gravityIcon.name}
@@ -47,14 +45,6 @@ export function ColetaAdminCard({ data, ...rest }: Props) {
             size="lg"
             mt={2}
           />
-        </HStack>
-        <HStack justifyContent="space-between" width="100%" px={2}>
-          <Text fontSize="xs" fontFamily="body" color={statusIcon.color} numberOfLines={1}>
-            Autor nº {author}
-          </Text>
-          <Text fontSize="xs" fontFamily="body" color={statusIcon.color} numberOfLines={1}>
-             Data: {showDate}
-          </Text>
         </HStack>
       </VStack>
 
--- frontend/src/screens/GerenciaColeta.tsx
+++ frontend/src/screens/GerenciaColeta.tsx
@@ -1,30 +1,24 @@
 import React, { useCallback, useState } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
 import { RefreshControl } from 'react-native';
-import { HStack, VStack, FlatList, useToast, Text, Heading, View } from "native-base";
+import { VStack, FlatList, useToast, Text, Heading, View, HStack, Pressable, Icon } from "native-base";
 
 import { IconHeader } from "@components/IconHeader";
 import { Loading } from '@components/Loading';
 import { AppError } from '@utils/AppError';
 import { api } from '@services/api';
-
-import { useAuth } from '@hooks/useAuth';
 import { ColetaAdminCard } from '@components/ColetaAdminCard';
-import { DenunciaDTO } from '@dtos/DenunciaDTO';
+import { ColetaDTO } from '@dtos/ColetaDTO';
 
 export function GerenciaColeta() {
   const [isLoading, setIsLoading] = useState(true);
-  const [complaint, setComplaint] = useState<DenunciaDTO[]>([]);
-  const video = React.useRef(null);
-
-  const { user } = useAuth();
-
+  const [complaint, setComplaint] = useState<ColetaDTO[]>([]);
   const toast = useToast();
 
   async function fetchDenuncias() {
     setIsLoading(true);
     try {
-      const { data } = await api.get('api/complaint');
+      const { data } = await api.get('api/collect');
       setComplaint(data);
     } catch (error) {
       const isAppError = error instanceof AppError;
@@ -49,12 +43,12 @@ export function GerenciaColeta() {
     <VStack flex={1}>
       <IconHeader title="Gerenciamento de Coletas" />
       {isLoading ? <Loading /> :
-        <View flex={1} m={3}>
+        <View flex={1} m={3} mt={2} mb={2}>
           <VStack flex={1} rounded="lg" bgColor="blue.400">
             <VStack mr={2} ml={2} flex={1}>
               <FlatList
-              mt={2}
-              mb={2}
+                mt={2}
+                mb={2}
                 refreshControl={
                   <RefreshControl
                     refreshing={isLoading}
@@ -64,7 +58,7 @@ export function GerenciaColeta() {
                 data={complaint}
                 keyExtractor={item => item.id?.toString() ?? ''}
                 renderItem={({ item }) => (
-                  <ColetaAdminCard data={item} onPress={ () => {}}/>
+                  <ColetaAdminCard data={item} onPress={() => { }} />
                 )}
                 ListEmptyComponent={() => (
                   <Text color="white" textAlign="center" fontFamily='body' fontSize="md" >
@@ -77,6 +71,49 @@ export function GerenciaColeta() {
           </VStack>
         </View>
       }
+      <VStack h="65%" bgColor="blue.400" rounded="lg" m={3} mt={0} mb={2}>
+        <Heading mt={2} textAlign="center" color="green.700" borderWidth={1}>
+          descrição
+        </Heading>
+        <VStack borderWidth={1}>
+          <Text ml={2}>
+            Autor
+          </Text>
+        </VStack>
+
+        <HStack borderWidth={1} justifyContent="center">
+          <Text borderWidth={1} m={1}>
+            tipo
+          </Text>
+          <Text borderWidth={1} m={1}>
+            status
+          </Text>
+          <Text borderWidth={1} m={1}>
+            gravidade
+          </Text>
+        </HStack>
+        <VStack>
+          <Text borderWidth={1} h={56} textAlign="center">
+            imagem
+          </Text>
+        </VStack>
+        <HStack px={1} py={1} mt={2} bgColor="blue.400" rounded="lg" justifyContent="space-evenly" borderWidth={1}>
+          <HStack justifyContent="space-evenly" flex={1}>
+            <Pressable  p={2}  _pressed={{ rounded: 9, bgColor: 'blue.300' }}
+              onPress={() => {}}>
+              <Icon alignSelf="center" />
+              <Text>Editar</Text>
+            </Pressable>
+
+            <Pressable p={2}  _pressed={{ rounded: 9, bgColor: 'blue.300' }}
+              onPress={() => {}}>
+              <Icon alignSelf="center" />
+              <Text>Deletar</Text>
+            </Pressable>
+          </HStack>
+        </HStack>
+
+      </VStack>
     </VStack >
   );
 
